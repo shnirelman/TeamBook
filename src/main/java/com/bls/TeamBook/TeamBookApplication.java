bb7457508b6dbd88041a8788fabac54ddfef9f69
Alexey <shnirelmana@gmail.com>
2024-02-23 09:38:42 +0400

    initial commit

---
+++ src/main/java/com/bls/TeamBook/TeamBookApplication.java
@@ -0,0 +1,13 @@
+package com.bls.TeamBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bls/TeamBook/TeamBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bls.TeamBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
