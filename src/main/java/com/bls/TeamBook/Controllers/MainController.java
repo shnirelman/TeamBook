5a8166e8117c9cccc62eff68da0d9de61c379018
BrovkoRoman <brovkora@rambler.ru>
2024-03-11 16:42:55 +0400

    add login and password validation

--- src/main/java/com/bls/TeamBook/Controllers/MainController.java
+++ src/main/java/com/bls/TeamBook/Controllers/MainController.java
@@ -5,6 +5,7 @@ import com.bls.TeamBook.models.MyUser;
 import com.bls.TeamBook.models.Question;
 import com.bls.TeamBook.repo.AnswerRepository;
 import com.bls.TeamBook.repo.QuestionRepository;
+import com.bls.TeamBook.repo.UserRepository;
 import com.bls.TeamBook.services.MainService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,35 +14,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @Controller
 @AllArgsConstructor
 public class MainController {
     private MainService service;
+    public String getLogin() {
+        String login = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        return login;
+    }
 
     @GetMapping("/")
     public String home(Model model) {
-        String login = SecurityContextHolder.getContext().getAuthentication().getName();
-        model.addAttribute("login", login);
+        model.addAttribute("login", getLogin());
 
         return "home";
     }
 
     @GetMapping("/tests")
     public String tests(Model model) {
-        String login = SecurityContextHolder.getContext().getAuthentication().getName();
-        model.addAttribute("login", login);
+        model.addAttribute("login", getLogin());
 
         return "tests";
     }
 
     @GetMapping("/article/{name}")
     public String home(@PathVariable(value = "name") String name, Model model) {
-        String login = SecurityContextHolder.getContext().getAuthentication().getName();
-        model.addAttribute("login", login);
+        model.addAttribute("login", getLogin());
 
         model.addAttribute("name", name);
         return "article";
@@ -51,11 +57,12 @@ public class MainController {
     private QuestionRepository questionRepository;
     @Autowired
     private AnswerRepository answerRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/test/{name}")
     public String test(@PathVariable(value = "name") String name, Model model) {
-        String login = SecurityContextHolder.getContext().getAuthentication().getName();
-        model.addAttribute("login", login);
+        model.addAttribute("login", getLogin());
 
         model.addAttribute("name", name);
         Iterable<Question> questions = questionRepository.findAll();
@@ -66,8 +73,38 @@ public class MainController {
     }
 
     @PostMapping("/new-user")
-    public String addUser(@RequestParam String login, @RequestParam String password,
+    public String addUser(@RequestParam String login, @RequestParam String password, @RequestParam String confirmPassword,
                           HttpServletRequest request, Model model) throws ServletException {
+        if(userRepository.findByLogin(login).isPresent()) {
+            model.addAttribute("loginError", "Такой пользователь уже есть");
+            model.addAttribute("login", getLogin());
+
+            return "new_user";
+        }
+
+        int len = login.length();
+
+        if(len == 0 || len > 20) {
+            model.addAttribute("loginError", "Логин должен быть от 1 до 20 символов");
+            model.addAttribute("login", getLogin());
+
+            return "new_user";
+        }
+
+        if(password.length() < 8) {
+            model.addAttribute("passwordError", "Пароль должен быть не менее 8 символов");
+            model.addAttribute("login", getLogin());
+
+            return "new_user";
+        }
+
+        if(!Objects.equals(confirmPassword, password)) {
+            model.addAttribute("confirmPasswordError", "Пароли не совпадают");
+            model.addAttribute("login", getLogin());
+
+            return "new_user";
+        }
+
         service.addUser(new MyUser(login, password));
 
         request.logout();
@@ -77,8 +114,7 @@ public class MainController {
 
     @GetMapping("/new-user")
     public String newUser(Model model) {
-        String login = SecurityContextHolder.getContext().getAuthentication().getName();
-        model.addAttribute("login", login);
+        model.addAttribute("login", getLogin());
 
         return "new_user";
     }
