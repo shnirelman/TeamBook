db11e969495579801f38bb6d614071ddab11b77f
Alexey <shnirelmana@gmail.com>
2024-05-17 16:45:20 +0400

    small bugfix in tabe of contents

--- src/main/resources/static/js/tableOfContents.js
+++ src/main/resources/static/js/tableOfContents.js
@@ -2,8 +2,7 @@ table = document.querySelector('#table_of_contents');
 
 let ol = document.createElement('ol');
 for(let chapter of chapters) {
-    ol.innerHTML += "<li><a href=" + chapter.code + ">" + chapter.name + "</a></li>";
-    console.log("<li><a href=" + chapter.code + ">" + chapter.name + "</a></li>");
+    ol.innerHTML += "<li><a href=#" + chapter.code + ">" + chapter.name + "</a></li>";
 }
 
 table.appendChild(ol);
